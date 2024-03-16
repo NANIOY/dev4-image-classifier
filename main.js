30d273d2401aeba7c78fc63de281baff772acf99
Nanio <111894274+NanioY@users.noreply.github.com>
2024-03-16 17:48:52 +0100

    docs: add comments

--- main.js
+++ main.js
@@ -9,9 +9,14 @@ let classifier;
 // setup
 async function setup() {
   try {
+    // load the model
     classifier = await ml5.imageClassifier('MobileNet');
     console.log('Model Loaded!');
+
+    // update status
     document.querySelector(".header__model--status").innerHTML = "Model loaded. You can now capture a picture.";
+
+    // enable capture button
     captureButton.removeAttribute('disabled');
   } catch (error) {
     console.error("Failed to load model:", error);
@@ -28,45 +33,48 @@ webcam.start()
 
 // capture photo and classify
 async function captureAndClassify() {
+  // check if video stream is available
   const video = webcamElement;
-
   if (!video.srcObject) {
     console.error("No video stream available.");
     return;
   }
 
+  // capture picture
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d');
-
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
-
   context.scale(-1, 1); // flip horizontally
   context.drawImage(video, 0, 0, canvas.width * -1, canvas.height);
 
   try {
+    // convert to blob
     canvas.toBlob(async function (blob) {
       if (!blob) {
         console.error("Failed to capture image as blob.");
         return;
       }
 
-      console.log('Captured picture:', blob);
-
+      // create image element
       const imageURL = URL.createObjectURL(blob);
       const imgElement = document.createElement('img');
       imgElement.src = imageURL;
 
+      // create card
       const cardDiv = document.createElement('div');
       cardDiv.classList.add('card');
 
+      // create image container and add image
       const imageContainer = document.createElement('div');
       imageContainer.classList.add('image-container');
       imageContainer.appendChild(imgElement);
 
+      // create container for classification results
       const resultContainer = document.createElement('div');
       resultContainer.classList.add('result-container');
 
+      // display results
       const results = await classifier.classify(canvas);
       console.log('Classification Results:', results);
       results
@@ -80,6 +88,7 @@ async function captureAndClassify() {
       cardDiv.appendChild(imageContainer);
       cardDiv.appendChild(resultContainer);
 
+      // append card to captured image container
       const capturedImageContainer = document.getElementById('capturedImage');
       capturedImageContainer.appendChild(cardDiv);
     }, 'image/jpeg');
